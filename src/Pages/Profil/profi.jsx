64db8142c6a6c9ad9e3fd9fced2a2c1bce8315ef
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-06-24 02:58:27 +0200

    Fin mise en place des 2 bannières

--- src/Pages/Profil/profi.jsx
+++ src/Pages/Profil/profi.jsx
@@ -7,33 +7,37 @@ import Square from "../../Components/Square";
 function Profil() {
   return (
     <div>
-      
+
       <Banner classNameBanner="top-banner">
-      <Nav
+        <Nav
           classContainer="nav-container-top"
           classContainerList="nav-container-top-list"
           classContainerListItem="nav-container-top-list-item"
-           elem1={"Accueil"}
-           elem2={"Profil"}
-           elem3={"Réglage"}
-           elem4={"Communauté"}>
-      <Logo/>
- 
-      </Nav>
+          elem1={"Accueil"}
+          elem2={"Profil"}
+          elem3={"Réglage"}
+          elem4={"Communauté"}>
+          <Logo />
+
+        </Nav>
       </Banner>
       <Banner classNameBanner="bottom-banner">
-      <Nav
+        <Nav
           classContainer="nav-container-bottom"
           classContainerList="nav-container-bottom-list"
           classContainerListItem="nav-container-bottom-list-item"
-           elem1={<Square/>}
-           elem2={<Square/>}
-           elem3={<Square/>}
-           elem4={<Square/>}>
-      
-      <p className="nav-container-bottom-list-item copyright">Copiryght, SportSee 2020</p>
-      </Nav>
-      
+
+          elem1={<Square logo={require('../../assets/gym.png')} />}
+          elem2={<Square logo={require('../../assets/bike.png')} />}
+          elem3={<Square logo={require('../../assets/swim.png')} />}
+          elem4={<Square logo={require('../../assets/yoga.png')} />}
+
+        >
+
+
+          <p className="nav-container-bottom-list-item copyright">Copiryght, SportSee 2020</p>
+        </Nav>
+
       </Banner>
     </div>
   );
