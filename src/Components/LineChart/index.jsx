f0ae3ce1a845d2dd41e6dfc2333b2e66741c0fe6
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-06-28 20:41:21 +0200

    mise en place de la LineChart

--- src/Components/LineChart/index.jsx
+++ src/Components/LineChart/index.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-function LineChart() {
-    return(
-    <div className="lineChart graphics-shape">
 
+const data = [
+  { name: 'L', minutes: 30 },
+  { name: 'M', minutes: 45 },
+  { name: 'M', minutes: 50 },
+  { name: 'J', minutes: 40 },
+  { name: 'V', minutes: 60 },
+  { name: 'S', minutes: 70 },
+  { name: 'D', minutes: 50 },
+];
+
+const LineChartComponent = () => {
+  return (
+    <div className="graphics-shape">
+      <h3>Durée moyenne des sessions</h3>
+      <ResponsiveContainer width="100%" height={250}>
+        <LineChart
+          data={data}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        >
+
+          <XAxis dataKey="name" />
+          <YAxis hide />
+          <Tooltip formatter={(value) => `${value} min`} />
+          <Legend verticalAlign="top" align="right" wrapperStyle={{ marginBottom: '20px' }} />
+          <Line type="monotone" dataKey="minutes" stroke="#8884d8" strokeWidth={2} />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
-)}
+  );
+};
 
-export default LineChart;
+export default LineChartComponent;
