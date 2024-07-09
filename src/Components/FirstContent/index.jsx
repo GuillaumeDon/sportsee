dba83fbd7311cb9b18fda1065940c259fbca86d4
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-07-09 09:12:07 +0200

    maj style

--- src/Components/FirstContent/index.jsx
+++ src/Components/FirstContent/index.jsx
@@ -14,10 +14,28 @@ const data = [
   { poids: 69.2, calories: 70.5 },
 ];
 
+function CustomLegend() {
+  return (
+    <div className="custom-legend">
+      <div className="legend-item">
+        <span className="legend-point black"></span>
+        Poids (kg)
+      </div>
+      <div className="legend-item">
+        <span className="legend-point red"></span>
+        Calories brûlées (kcal)
+      </div>
+    </div>
+  );
+}
+
 function FirstContent() {
   return (
     <div className="resultats-content-1">
-      <h3>Activité quotidienne</h3>
+      <div className="header">
+        <h3>Activité quotidienne</h3>
+        <CustomLegend />
+      </div>
       <ResponsiveContainer width={835} height="100%">
         <BarChart
           width={500}
@@ -28,9 +46,8 @@ function FirstContent() {
         >
           <CartesianGrid vertical={false} />
           <XAxis dataKey="day" />
-          <YAxis yAxisId="right" orientation="right" domain={[69, 71]} ticks={[69, 70]} />
+          <YAxis yAxisId="right" orientation="right" domain={[69, 71]} ticks={[69,70, 71]} />
           <Tooltip />
-          <Legend verticalAlign="top" align="right" wrapperStyle={{ marginBottom: '20px' }} />
           <Bar yAxisId="right" dataKey="poids" fill="#000000" />
           <Bar yAxisId="right" dataKey="calories" fill="#FF0000" />
         </BarChart>
