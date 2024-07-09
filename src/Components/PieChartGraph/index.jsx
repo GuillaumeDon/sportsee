f785903ad5c169b163831f2bbc37829fba59ec2d
Guillaume Donizetti <glm.donizetti@gmail.com>
2024-07-09 08:32:52 +0200

    mise en place piechart

--- src/Components/PieChartGraph/index.jsx
+++ src/Components/PieChartGraph/index.jsx
@@ -1,11 +1,12 @@
 import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Cell } from 'recharts';
 
 const data = [
-  { name: 'Group A', value: 100 },
-
+  { name: 'Filled', value: 12 },
+  // { name: 'Unfilled', value: 88 },
 ];
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const COLORS = ['#0088FE', 'transparent']; // Utiliser 'transparent' pour la partie non remplie
 
 export default class Example extends PureComponent {
   static demoUrl = 'https://codesandbox.io/s/pie-chart-with-padding-angle-7ux0o';
@@ -15,19 +16,17 @@ export default class Example extends PureComponent {
       <PieChart className="pie-chart-container graphic-shapes" width={256} height={268} onMouseEnter={this.onPieEnter}>
         <Pie
           data={data}
-          cx={120}
-          cy={200}
+          startAngle={180}
+          endAngle={180 - (360 * (data[0].value / 100))}
           innerRadius={60}
           outerRadius={80}
           fill="#8884d8"
-          paddingAngle={5}
           dataKey="value"
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
           ))}
         </Pie>
-
       </PieChart>
     );
   }
